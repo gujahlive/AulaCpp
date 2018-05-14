140f5d08363923db98b76e82e5cc81ea72cc4172
Gustavo Ferreira <gujahlive@users.noreply.github.com>
2018-05-14 20:59:48 -0300

    somando
    cout<< cin>>

---
+++ somaCpp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int i=1,i1=11,n1,n2,soma;
+	
+	cout<<"\t\tSoma de numeros\n";
+	cout<<"Digite o primeiro numero: ";
+	cin>>n1;
+	cout<<"Digite o primeiro numero: ";
+	cin>>n2;
+	soma = n1 + n2 ;
+	cout<<"A soma é: " << soma;
+	
+	
+	
+	return 0;
+}
