cdbb9aeec56a9946c3f78056b86230666fa861c1
Gustavo Ferreira <gujahlive@users.noreply.github.com>
2018-05-14 21:08:43 -0300

    subindo

---
+++ até 1000.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,n;
+	for(i=0;i<1000; i++){
+		cout<<"num: "<<i+1;
+		cout<<"\n";
+	}
+	
+	
+	return 0;
+}
---
+++ só os pares.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int i,n;
+	for(i=0;i < 10 ;i=i+1){
+		if(i%2==0){
+			cout<<"num: "<<i+2;
+			cout<<"\n";	
+		}
+		
+	}
+	
+	
+	return 0;
+}
